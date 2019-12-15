805815d6b4a8e161b637b1d6d3ab8523dd44fe19
Hylson Kobayashi <hylsonk@gmail.com>
2019-12-15 14:37:35 -0300

    query de apagar todo DWsakila

---
+++ removeTudo.sql
@@ -0,0 +1,7 @@
+DELETE FROM rental WHERE rental_id > 0;
+DELETE FROM staff WHERE staff_id > 0;
+DELETE FROM customer WHERE customer_id > 0;
+DELETE FROM film WHERE film_id > 0;
+DELETE FROM date WHERE date_id > 0;
+DELETE FROM language WHERE language_id > 0;
+DELETE FROM store WHERE store_id > 0;
